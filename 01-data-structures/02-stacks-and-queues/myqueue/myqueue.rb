bb17cc6aa1d4c6af25e9cca33d9a3f9f330ba78a
blunce24 <blairlunceford@gmail.com>
2017-03-23 16:19:16 -0600

    Completed Stacks and Queues Checkpoint

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,30 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    if @head == nil
+      @queue.insert(0, element)
+      @head = @queue[0]
+      @tail = @queue[0]
+    else
+      @queue.insert(@queue.length, element)
+      @head = @queue[-1]
+      @tail = @queue[0]
+    end
   end
 
   def dequeue
+    value = @tail
+    @tail = @queue[1]
+    @queue.delete(value)
+    @head = @queue[-1]
+    value
   end
 
   def empty?
+    @head.nil? && @tail.nil?
   end
 end
